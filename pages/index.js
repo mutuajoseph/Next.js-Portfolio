7531c4112c4f5a62970bc96fa46419d7f3ad7057
mutuajoseph <wambuajoseph756@gmail.com>
2021-03-05 18:28:36 +0300

    added media queries to factor small screens

--- pages/index.js
+++ pages/index.js
@@ -148,8 +148,7 @@ export default function Home() {
                   target="_blank"
                   rel="noopener noreferrer"
               >
-                  Powered by{' '}
-                  <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+                  Designed by{' '} Wambua.
               </a>
           </footer>
       </div>
