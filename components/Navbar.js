a6b8dcbbf00fde5317eaf800fbce3077896899e6
mutuajoseph <wambuajoseph756@gmail.com>
2021-03-05 18:24:23 +0300

    added media queries to factor small screens

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,9 +8,7 @@ export default  function Navbar () {
                 <h2 className={navStyles.h2}>J<span className={navStyles.bgBlue}>W.</span></h2>
                 <div className={navStyles.flex}>
                     <ul className={navStyles.list}>
-                        <li className={navStyles.listItem}>About</li>
                         <li className={navStyles.listItem}>CV</li>
-
                     </ul>
                     <button className={navStyles.sayHi}>Say Hi!</button>
                 </div>
